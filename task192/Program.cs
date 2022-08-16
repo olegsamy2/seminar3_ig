c8bb8ce7d3014b3f0ff2d9c7ba864a7c3102eacc
OlegSam2 <doorsmania@mail.ru>
2022-08-16 21:45:00 +0300

    homework palindrome second  way

---
+++ task192/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.	
+// 14212 -> нет	
+// 12821 -> да	
+// 23432 -> да	
+
+//   2 способ - цикл
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numbersave = number;
+int reversenumber = 0;
+
+if (number > 9999 && number < 100000)
+
+{
+while (number > 0)
+{
+    int dig = number % 10;   // 1 итерация - последняя цифра числа, 2 итерация - предпоследняя...
+    reversenumber = reversenumber * 10 + dig;  // переворот числа 
+    number = number / 10;  // действие для выполнения цикла
+}
+ 
+ if (numbersave == reversenumber)
+ {
+    Console.WriteLine(numbersave + " -> да, это палиндром");
+    }
+else
+{
+    Console.WriteLine(numbersave + " -> нет, это не палиндром");
+}
+    }
+else
+{
+    Console.WriteLine("Введено не пятизначное число");
+}
